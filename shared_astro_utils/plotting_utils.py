fb94525a1ba3f6f4e676790b2cd000ab02c2bd22
Mike Walmsley <walmsleymk1@gmail.com>
2019-01-10 11:40:04 +0000

    Make labels big

--- shared_astro_utils/plotting_utils.py
+++ shared_astro_utils/plotting_utils.py
@@ -15,13 +15,10 @@ def plot_galaxy_grid(galaxies, rows, columns, save_loc, labels=None):
             galaxy = galaxies[n, :, :, :]  # n, x, y, channel, in ML style
             data = galaxy.squeeze()
             ax.imshow(data.astype(np.uint8))
-            ax.text(0.2, 0.2, labels[n], transform=ax.transAxes)
-            # ax.grid(False)
-            # ax.get_xaxis().set_visible(False)
-            # ax.get_yaxis().set_visible(False)
-            # TODO add labels
-            # label_str = '{:.2}'.format(label)
-            # ax.text(60, 110, label_str, fontsize=16, color='r')
+            ax.text(0.2, 0.2, labels[n], transform=ax.transAxes, color='red', fontsize=16)
+            ax.grid(False)
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
         # plt.tight_layout()
         plt.savefig(save_loc, bbox_inches='tight')
         plt.close()

db6810f0fda403208db2d57229bd4138528ac37f
Mike Walmsley <walmsleymk1@gmail.com>
2019-01-10 11:35:21 +0000

    Add labels

--- shared_astro_utils/plotting_utils.py
+++ shared_astro_utils/plotting_utils.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
       
 
-def plot_galaxy_grid(galaxies, rows, columns, save_loc):
+def plot_galaxy_grid(galaxies, rows, columns, save_loc, labels=None):
         fig = plt.figure(figsize=(columns * 4, rows * 4))  # x, y order
         gs1 = gridspec.GridSpec(rows, columns, fig)  # rows (y), cols (x) order
         gs1.update(wspace=0.025, hspace=0.025)
@@ -15,6 +15,7 @@ def plot_galaxy_grid(galaxies, rows, columns, save_loc):
             galaxy = galaxies[n, :, :, :]  # n, x, y, channel, in ML style
             data = galaxy.squeeze()
             ax.imshow(data.astype(np.uint8))
+            ax.text(0.2, 0.2, labels[n], transform=ax.transAxes)
             ax.grid(False)
             ax.get_xaxis().set_visible(False)
             ax.get_yaxis().set_visible(False)
